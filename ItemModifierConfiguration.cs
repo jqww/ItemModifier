44a4965fc14e437a1b18c11a8a1b8ab4a42532d8
Moontiz <53201564+jqww@users.noreply.github.com>
2021-01-13 00:22:25 -0600

    Add files via upload

---
+++ ItemModifierConfiguration.cs
@@ -0,0 +1,35 @@
+﻿using Rocket.API;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+
+namespace ItemModifier
+{
+    public class ItemModifierConfiguration : IRocketPluginConfiguration
+    {
+        public bool LoadAfterWorkshop { get; set; }
+        [XmlArrayItem(ElementName = "Item")]
+        public List<ItemModification> Items { get; set; }
+
+        public void LoadDefaults()
+        {
+            LoadAfterWorkshop = false;
+            Items = new List<ItemModification>()
+            {
+                new ItemModification()
+                {
+                    ID = 363,
+                    PlayerDamage = 40,
+                    PlayerLegMultiplier = 0.6f,
+                    PlayerArmMultiplier = 0.6f,
+                    PlayerSpineMultiplier = 0.8f,
+                    PlayerSkullMultiplier = 1.1f
+                },
+                new ItemModification()
+                {
+                    ID = 310,
+                    Armor = 0.7f
+                }
+            };
+        }
+    }
+}
